a3ff2cdb18cacff331cb394f99395fc97ed70d19
111Yokoo <yokoomatheus@gmail.com>
2024-10-11 14:18:33 -0300

    Footer

---
+++ resonance-loja/src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaSquareYoutube } from "react-icons/fa6";
+import "./styles.css";
+
+const Footer = ({corTexto, corBackground, corSecundaria}) => {
+    return (
+    <footer style={{backgroundColor: corBackground}}>
+        <section className="containerLink">
+            <ul className="linksExternos">
+                <a href="www.instagram.com/">
+                    <li style={{color: corSecundaria}}><FaSquareInstagram /></li>
+                </a>
+                <a href="https://www.facebook.com/">
+                    <li style={{color: corSecundaria}}><FaFacebookSquare /></li>
+                </a>
+                <a href="https://mail.google.com/mail">
+                    <li style={{color: corSecundaria}}><MdEmail /></li>
+                </a>
+                <a href="https://www.youtube.com">
+                    <li style={{color: corSecundaria}}><FaSquareYoutube /></li>
+                </a>
+            </ul>
+        </section>
+        <section className="containerMeio">
+            <p style={{color: corTexto}}>Todos os direitos reservados &copy; Nexgen</p>
+        </section>
+        <section className="containerFim">
+            <p style={{color: corTexto}}><a href="#" style={{color: corTexto}}>Privacy Policy</a> | <a href="#" style={{color: corTexto}}>Termos e condições</a></p>
+        </section>
+    </footer>
+  );
+};
+
+
+export default Footer;
--- resonance-loja/src/pages/Home.js
+++ resonance-loja/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Header/>
       <Banner />
       <h1>Home</h1>
-      
+      <Footer corTexto="#fff" corBackground="#000" corSecundaria="#6f5f40"/>
     </div>
   );
 }
